e7518e0bca0e3ea8cf3e6f7b1e8e33910eb2acf8
werfy <michael.griffiths@surefiresystems.com>
2018-02-23 14:32:35 +1100

    removed trailing spaces and added newline at end of file

--- routes/health.py
+++ routes/health.py
@@ -1,12 +1,11 @@
 from flask_restful import Resource
-import requests
 
 class Health(Resource):
 
     def get(self):
 
         status = "DOWN"
-        response = 500   
+        response = 500
 
         health = self.checkHealth()
 
@@ -16,6 +15,6 @@ class Health(Resource):
 
         return {'Status': status}, response
 
-    
+
     def checkHealth(self):
         return True
