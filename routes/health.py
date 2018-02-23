972c12d783b24b1fb84b43a05de2ebfa6510a2ee
werfy <michael.griffiths@surefiresystems.com>
2018-02-23 17:15:13 +1100

    Reworked Healthcheck to allow testing of failed health.

--- routes/health.py
+++ routes/health.py
@@ -2,19 +2,33 @@ from flask_restful import Resource
 
 class Health(Resource):
 
+    """
+        Health check service responding at /health
+        Users will query the health check endpoint to confirm that app is in a usable state.
+        Health will return a server error when the service is not healthy.
+    """
+
     def get(self):
 
-        status = "DOWN"
-        response = 500
-
-        health = self.checkHealth()
-
-        if health:
+        if self.check_health():
             response = 200
-            status = "UP"
+            status_text = "UP"
+        else:
+            response = 500
+            status_text = "DOWN"
 
-        return {'Status': status}, response
+        return {'Status': status_text}, response
 
+    def check_health(self, healthy=True):
 
-    def checkHealth(self):
-        return True
+        """
+            Currently only returns a healthy status unless a false healthy state is passed in
+            (which can be used for unit testing).
+            This function should be expanded to check the status of downstream services such as
+            queues, databases and external endpoints as they are added to the app.
+        """
+
+        if not healthy:
+            return False
+
+        return healthy
